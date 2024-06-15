a223019e0e077ca25eb94f03ccebb934f04969b6
Mateo Martinez Marulanda <mateomm01@gmail.com>
2024-06-15 17:12:29 -0500

    Chaos monkey

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,12 @@
 
 #---------------------------------------------------------------------------------------------------------------#
 
-FROM openjdk:17
-COPY target/ApiRest-0.0.1-SNAPSHOT.jar ApiRest-0.0.1-SNAPSHOT.jar
+#FROM openjdk:17
+#COPY target/ApiRest-0.0.1-SNAPSHOT.jar ApiRest-0.0.1-SNAPSHOT.jar
 #COPY ./target/employee-jdbc-0.0.1-SNAPSHOT.jar employee-jdbc-0.0.1-SNAPSHOT.jar
-CMD ["java", "-jar", "ApiRest-0.0.1-SNAPSHOT.jar"]
+#CMD ["java", "-jar", "ApiRest-0.0.1-SNAPSHOT.jar"]
+
+FROM openjdk:17
+ADD target/ApiRest-0.0.1-SNAPSHOT.jar ApiRest-0.0.1-SNAPSHOT.jar
+EXPOSE 8080
+ENTRYPOINT ["java","-jar","ApiRest-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/example/ApiRest/Health/CustomHealthIndicator.java
@@ -0,0 +1,28 @@
+package com.example.ApiRest.Health;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+
+    @Override
+    public Health health() {
+        int errorCode = check(); // Implementa tu lógica de verificación aquí
+        if (errorCode != 0) {
+            return Health.down()
+                    .withDetail("Error Code", errorCode)
+                    .withDetail("Description", "El componente ha fallado con un código de error.")
+                    .build();
+        }
+        return Health.up()
+                .withDetail("message", "El Health Check está funcionando correctamente.")
+                .build();
+    }
+
+    private int check() {
+        // Lógica para revisar un recurso, como una base de datos
+        return 0; // 0 significa que está saludable
+    }
+}
